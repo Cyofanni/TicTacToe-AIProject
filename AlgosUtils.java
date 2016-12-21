c95d333818abdde34bf0b7d8c7f823f0a5b1f836
abeccaro <alex.becks@hotmail.it>
2016-12-21 15:35:32 +0100

    Updated to ticTacToe.Move class
    Added imports and set abstract type

--- AlgosUtils.java
+++ AlgosUtils.java
@@ -1,10 +1,14 @@
+import ticTacToe.AbsTicTacToe;
+import ticTacToe.AbsMove;
+import ticTacToe.Move;
+
 public class AlgosUtils{
-	public ArrayList<Move> computeActions(char [][] board){
-		 ArrayList<Move> resActions = new ArrayList<Move>();
+	public ArrayList<AbsMove> computeActions(char [][] board){
+		 ArrayList<AbsMove> resActions = new ArrayList<AbsMove>();
 		 for (int i = 0; i < board.length; i++){
-			for (int j = 0; i < board[].length; j++){
+			for (int j = 0; i < board[i].length; j++){
 				if (board[i][j] == AbsTicTacToe.BLANK_VALUE){
-					resActions.add(new Move(i, j));		
+					resActions.add(new Move(i, j));
 				}
 			}
 		 }
