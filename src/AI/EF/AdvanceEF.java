201ce1c21eb1ea03566cef5baacdfb157014514b
Davide Rigoni <davider1994@gmail.com>
2017-09-04 15:04:44 +0200

    Fixed the evaluation function

--- src/AI/EF/AdvanceEF.java
+++ src/AI/EF/AdvanceEF.java
@@ -28,9 +28,8 @@ public class AdvanceEF  implements IEvalFunction{
             }
         } else{
 
-            /*if(length%2 == 1){
+            if(length%2 == 1){
                 int index = (length/2);
-                System.out.println(index);
                 if(field[index][index] == AbsTicTacToe.PLAYERSVALUE[1]){
                     plus = length;
                 } else{
@@ -40,11 +39,11 @@ public class AdvanceEF  implements IEvalFunction{
                         plus = 0;
                     }
                 }
-            }*/
+            }
 
             double valAI = AIUtils.SimpleAIAlgorithm(field,1);
             double valPlayer = AIUtils.SimpleAIAlgorithm(field,0);
-            return valAI - valPlayer;
+            return valAI - valPlayer + plus;
         }
 
 
